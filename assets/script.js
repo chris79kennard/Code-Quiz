ce0241dc7faebb1ae51c155aca0919836da53fef
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-09-26 13:37:58 -0500

    finalized CSS sheet

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// set a variable for start btn to start the game and make button functional
+const startButton = document.getElementById("Start-btn");
+startButton.addEventListener("click", startGame);
+
+//  need to be able to start the game  NOT WORKING!!!!
+function startGame() {
+  console.log("Started");
+}
+// need to be able to move on to next question
+function setNextQuestion() {}
+// need to be able to select a answer
+function selectAnswer() {}
