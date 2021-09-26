83d9a123cb031fd3916f5126a188ac26aaa96c4f
Christopher Kennard <84753350+chris79kennard@users.noreply.github.com>
2021-09-26 14:18:38 -0500

    enabled function startGame via html

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,3 @@
-// set a variable for start btn to start the game and make button functional
-const startButton = document.getElementById("Start-btn");
-startButton.addEventListener("click", startGame);
-
 //  need to be able to start the game  NOT WORKING!!!!
 function startGame() {
   console.log("Started");
